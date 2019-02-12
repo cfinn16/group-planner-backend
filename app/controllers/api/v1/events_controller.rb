53437521990289e9ec20eb2bc80969c8cfd08778
cfinn16 <connorfinnegan16@gmail.com>
2019-02-12 16:58:23 -0500

    added update and delete for events, fixed CORS issue

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -9,6 +9,17 @@ class Api::V1::EventsController < ApplicationController
     render json: @event, status: :created
   end
 
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    render json: @event, status: :accepted
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.delete
+  end
+
   def event_params
     params.require(:event).permit(:name, :category, :description, :group_id)
   end
