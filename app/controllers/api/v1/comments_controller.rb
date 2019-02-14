c0038e06008a65832387d0914fb50d78b2cd78a3
jmartinez729 <jmartinez729@gmail.com>
2019-02-14 15:12:20 -0500

    added comments functionality

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -4,5 +4,14 @@ class Api::V1::CommentsController < ApplicationController
     render json: @comments
   end
 
+  def create
+    @comment = Comment.create(comment_params)
+    render json: @comment, status: :created
+  end
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :event_id, :content)
+  end
+
 
 end
